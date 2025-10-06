0e5fa8ca086f4413a6a7262506e600efefc560bc
rohman <111178009+rohmanBlue@users.noreply.github.com>
2025-10-06 16:23:07 +0800

    Delete apps/api/prisma/migrations/20240821200527_ directory

--- apps/api/prisma/migrations/20240821200527_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `categoryFavorite` on the `category` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `category` DROP COLUMN `categoryFavorite`;
