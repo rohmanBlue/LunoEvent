600208dd64ed1a9cc1c67d5984cf912d5ebbf248
rohman <111178009+rohmanBlue@users.noreply.github.com>
2025-10-11 08:42:02 +0800

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost:8000/' ], 
+    domains: ['images: {
+  domains: ['localhost', 'starfish-app-xrxoq.ondigitalocean.app'],
+}
+ ], 
   },
 }
 
