b63a26e6183e3313a8de4205926e99a60f9dc6f4
John Doe~ <johndoe@example.com>
2025-10-12 23:49:45 +0800

    feat: hallo ee so

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -93,7 +93,7 @@ export default class App {
   }
 
   public start(): void {
-    this.app.listen(port, () => {
+    this.app.listen(port, "0.0.0.0", () => {
       console.log(`  ➜  [API] Local: http://localhost:${port}/`);
     });
   }
