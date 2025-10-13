2577fbc7db8d9c18c3d18d522b8e4b09c8abe701
John Doe~ <johndoe@example.com>
2025-10-13 09:31:59 +0800

    feat: hallo update axios

--- apps/web/src/helper/apiCall.ts
+++ apps/web/src/helper/apiCall.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/', // Your API URL
+  baseURL: 'http://134.199.195.231:8000', // Your API URL
   headers: {
     'Content-Type': 'application/json',
   },
