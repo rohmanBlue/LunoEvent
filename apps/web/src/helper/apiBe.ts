10238e87767713214b67ae874efde1e5effef7ec
man <111178009+rohmanNEXT@users.noreply.github.com>
2025-11-12 14:33:11 +0800

    Refactor API base URL and update token handling

--- apps/web/src/helper/apiBe.ts
+++ apps/web/src/helper/apiBe.ts
@@ -1,24 +1,21 @@
 // src/api/axios.ts
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+ //   baseURL: 'http://localhost:8000/',
+  baseURL: 'http://134.199.195.231:8000', // Your API URL
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
-// interceptor otomatis tambahkan Bearer Token
 api.interceptors.request.use((config) => {
-  let token = null;
-  if (typeof window !== "undefined") {
-    token = localStorage.getItem("token");
-  }
-
+  const token = localStorage.getItem('token');
   if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
 });
 
 export default api;
+
